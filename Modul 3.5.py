c534bd0baf748229fca04cf11fb353c6366cc680
bulb0t <yu.uglenko@yandex.ru>
2024-10-01 21:46:30 +0300

    Update Modul 3.5.py

--- Modul 3.5.py
+++ Modul 3.5.py
@@ -3,6 +3,8 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+    elif first == 0:
+        return 1
     else:
         return first
 
